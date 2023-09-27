8ca814045c5adc968a98ddce2dbc5b03bff71b97
akashpatelknit <cotsec14@gmail.com>
2023-09-27 15:53:50 +0530

    fas

--- server/app.js
+++ server/app.js
@@ -38,6 +38,7 @@ app.use(
 app.use(function (req, res, next) {
 	//Enabling CORS
 	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Credentials', true);
 	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
 	res.header(
 		'Access-Control-Allow-Headers',
