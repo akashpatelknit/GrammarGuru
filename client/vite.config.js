4d00602017e1941a3d7dd60753ba051d3f6d5569
akashpatelknit <cotsec14@gmail.com>
2023-09-28 01:49:44 +0530

    first commite

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	scripts: {
+		dev: 'vite',
 		build: 'vite build',
-		preview: 'vite preview --port 8080',
+		preview: 'vite preview',
+		host: 'vite --host',
 	},
 });
